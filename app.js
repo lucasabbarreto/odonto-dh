4882cf6b2e8fa682295c456f4121339f9584c0d2
Lucas Barreto <lucasabbarreto@gmail.com>
2022-03-01 21:42:03 -0300

    página de cadastro adicionada

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
-
-
+var cadastroRouter = require('./routes/cadastro')
 
 var app = express();
 
@@ -25,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/cadastro', cadastroRouter);
 
 
 // catch 404 and forward to error handler
