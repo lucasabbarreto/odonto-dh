7c810a1e6cb97dc8dbddf46fc12ed501bf461582
Erval Souza <ervalsouza20@gmail.com>
2022-03-15 20:28:19 -0300

    Inicio da tela Admin

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var cadastroRouter = require('./routes/cadastro')
 
+// Rota Admin
+let adminRouter = require('./routes/admin/admin')
 var app = express();
 
 // view engine setup
@@ -26,14 +28,17 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/cadastro', cadastroRouter);
 
+// Admin
+app.use('/admin/acesso', adminRouter)
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
